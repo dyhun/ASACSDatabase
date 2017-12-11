734aefe1e76f642fd873c743335274a6d9063c91
Endre Szabo <endre.sti@gmail.com>
2017-12-11 23:45:18 +0200

    Adding Bugfender for remote logging

--- app/src/main/java/com/asacs/uav/asacsdatabase/MainActivity.java
+++ app/src/main/java/com/asacs/uav/asacsdatabase/MainActivity.java
@@ -3,11 +3,18 @@ package com.asacs.uav.asacsdatabase;
 import android.app.Activity;
 import android.os.Bundle;
 
+import com.bugfender.sdk.Bugfender;
+
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //Bugfender logging
+        Bugfender.init(this, "zYr3Yb4uaRu8aCa8lejEHlH57Xu6o4z8", BuildConfig.DEBUG);
+        Bugfender.enableLogcatLogging();
+        Bugfender.enableUIEventLogging(getApplication());
+        Bugfender.enableCrashReporting();
         setContentView(R.layout.activity_main);
     }
 }
